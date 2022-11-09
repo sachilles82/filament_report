5273e1712313e8330a6efcc90202f45d4bd3ce0d
sachilles <kina98@gmx.ch>
2022-11-09 22:12:17 +0100

    Filament Applikation erstellt und Dataseed gemacht

---
+++ lang/vendor/filament/cs/layout.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'direction' => 'ltr',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'Zapnout tmavý režim',
+        ],
+
+        'database_notifications' => [
+            'label' => 'Zobrazit notifikace',
+        ],
+
+        'light_mode' => [
+            'label' => 'Zapnout světlý režim',
+        ],
+
+        'logout' => [
+            'label' => 'Odhlásit se',
+        ],
+
+    ],
+
+];
